42c1ae6687c7c79abc48f4914df66d06b8c1f6a0
Arturo Espinosa <Arturo Espinosa@192.168.1.74>
2021-06-19 11:31:39 -0500

    Incial Commit

---
+++ Selenium/src/test/java/pruebaNG/NewTest.java
@@ -0,0 +1,12 @@
+package pruebaNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  Assert.assertTrue(false);
+  }
+}
